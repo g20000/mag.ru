bef2b046ed784822b51b9db9f6a2f5d8ab9ddba9
g20000 <pragmus2@gmail.com>
2015-10-19 10:07:01 +0300

    New ability: editable menu unit

--- www/pages/asideDropdownMenuPackage.php
+++ www/pages/asideDropdownMenuPackage.php
@@ -26,12 +26,40 @@
 	}
 	
 	function deleteCategory(id){
-		console.log(id);
 		$.ajax({
 					url: '<?php echo $cfg['options']['siteurl']; ?>/gears/ajax.deletePkgCatAsideMenu.php',
 					type: 'POST',
 					dataType: 'JSON',
 					data: {idItem:id},
+					success: function(data) {
+						console.log(data);
+						if (data.type=='error') {
+							notify(data.type, data.type, data.text);
+						} else{
+							document.location.reload();
+						}
+					},
+					error: function(v1,v2,v3) {
+						alert('Ошибка!\nПопробуйте позже.');
+						console.log(v1,v2,v3);
+					}
+				});
+	}
+	
+	function editCategory(idCat, nameCat){
+		var id = idCat;
+		var name = nameCat;
+		console.log(idCat);
+		console.log(nameCat);
+		$.ajax({
+					url: '<?php echo $cfg['options']['siteurl']; ?>/gears/ajax.updatePkgCatAsideMenu.php',
+					type: 'POST',
+					dataType: 'JSON',
+					data: {
+							idItem: idCat,
+							nameItem: nameCat
+							
+					},
 					success: function(data) {
 						console.log(data);
 						if (data.type=='error') {
@@ -61,7 +89,7 @@
   </div>
   <div class="checkbox">
     <label>
-      <input type="checkbox"> Иконка меню по умолчанию
+      <input type="checkbox" value="true"> Иконка меню по умолчанию
     </label>
   </div>
   <button type="button" class="btn btn-default" onclick="addCategory()">Добавить</button>
@@ -75,7 +103,10 @@
 	$menuItemList = getPackCategoriesForAsideMenu();
 	if(isset($menuItemList)){
 		foreach($menuItemList as $u){
-			echo '<p><span contenteditable="true">'.$u->name.'</span><button type="button" class="btn btn-danger btn-xs" onclick="deleteCategory('.$u->pkg_cat_ddlist_id.')">Удалить</button></p>';
+			?>
+			<p><span contenteditable="true"><?php echo $u->name ?></span><button type="button" class="btn btn-danger btn-xs" onclick="deleteCategory(<?php echo $u->pkg_cat_ddlist_id ?>)">Удалить</button>
+			<button type="button" class="btn btn-success btn-xs" onclick="editCategory(<?php echo $u->pkg_cat_ddlist_id ?>,<?php echo "'".$u->name."'" ?>)">Сохранить</button></p>
+			<?php
 		}
 	}
 ?>
